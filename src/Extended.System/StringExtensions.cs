21a4ea349976e2c85321e0bacb487214631a9a8f
Selectorsvt <selectorsvt@gmail.com>
2024-05-23 02:16:25 -0700

    Release version 1.0.0

---
+++ src/Extended.System/StringExtensions.cs
@@ -0,0 +1,89 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Extended.System
+{
+    /// <summary>
+    /// The string extensions class.
+    /// </summary>
+    public static class StringExtensions
+    {
+        private static Regex htmlTagsRegex = new Regex(@"<[^>]*>");
+
+        /// <summary>
+        /// Returns the enum using the specified value.
+        /// </summary>
+        /// <typeparam name="T">The .</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>The.</returns>
+        public static T ToEnum<T>(this string value) where T : Enum
+        {
+            return (T)Enum.Parse(typeof(T), value);
+        }
+
+        /// <summary>
+        /// Erases the html using the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The string.</returns>
+        public static string EraseHtml(this string value)
+        {
+            return htmlTagsRegex.Replace(value, string.Empty).Replace("&nbsp;", " ");
+        }
+
+        /// <summary>
+        /// Returns the bytes using the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <returns>The byte array.</returns>
+        public static byte[] ToBytes(this string value, Encoding? encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            return encoding.GetBytes(value);
+        }
+
+        /// <summary>
+        /// Hashes the hmac using the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <returns>The byte array.</returns>
+        public static byte[] HashHMAC(this string message, string key, Encoding? encoding = null)
+        {
+            var keyBytes = key.ToBytes(encoding);
+            return message.HashHMAC(keyBytes, encoding);
+        }
+
+        /// <summary>
+        /// Hashes the hmac using the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="keyBytes">The key bytes.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <returns>The byte array.</returns>
+        public static byte[] HashHMAC(this string message, byte[] keyBytes, Encoding? encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            var messageBytes = message.ToBytes(encoding);
+            var hash = new HMACSHA256(keyBytes);
+            return messageBytes.ComputedHash(hash);
+        }
+
+        /// <summary>
+        /// Gets the name without extension using the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>The string.</returns>
+        public static string GetNameWithoutExtension(this string path)
+        {
+            return Path.GetFileNameWithoutExtension(path);
+        }
+    }
+}
