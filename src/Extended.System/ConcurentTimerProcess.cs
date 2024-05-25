661115cfaf6c2ba8cfb5ae230fdcd415bbc86313
Selectorsvt <selectorsvt@gmail.com>
2024-05-25 03:59:51 -0700

    Add delegate extensions and concurent timer.

---
+++ src/Extended.System/ConcurentTimerProcess.cs
@@ -0,0 +1,124 @@
+using Timer = System.Timers.Timer;
+
+namespace Extended.System
+{
+    /// <summary>
+    /// The concurent timer process class.
+    /// </summary>
+    /// <seealso cref="IDisposable"/>
+    public class ConcurentTimerProcess : IDisposable
+    {
+        /// <summary>
+        /// The locker.
+        /// </summary>
+        private object _locker = new object();
+
+        /// <summary>
+        /// The timer.
+        /// </summary>
+        private Timer _timer;
+
+        /// <summary>
+        /// The action.
+        /// </summary>
+        private Action _action;
+
+        /// <summary>
+        /// The is disposed.
+        /// </summary>
+        private bool isDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConcurentTimerProcess"/> class.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <param name="timeSpan">The time span.</param>
+        public ConcurentTimerProcess(Action action, TimeSpan timeSpan)
+        {
+#if NET7_0_OR_GREATER
+            _timer = new Timer(timeSpan);
+#else
+            _timer = new Timer(timeSpan.TotalMilliseconds);
+#endif
+            _action = action;
+            _timer.Elapsed += Callback;
+        }
+
+        /// <summary>
+        /// Callbacks the sender.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The .</param>
+        private void Callback(object? sender, global::System.Timers.ElapsedEventArgs e)
+        {
+            var hasLock = false;
+
+            try
+            {
+                Monitor.TryEnter(_locker, ref hasLock);
+                if (!hasLock)
+                    return;
+
+                _action.Invoke();
+            }
+            finally
+            {
+                if (hasLock)
+                {
+                    Monitor.Exit(_locker);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts this instance.
+        /// </summary>
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        /// <summary>
+        /// Disposes this instance.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes the disposing.
+        /// </summary>
+        /// <param name="disposing">The disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (isDisposed) return;
+
+            if (disposing)
+            {
+                _timer.Elapsed -= Callback;
+                _timer.Dispose();
+            }
+
+            isDisposed = true;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="ConcurentTimerProcess"/> class.
+        /// </summary>
+        ~ConcurentTimerProcess()
+        {
+            // Finalizer calls Dispose(false)
+            Dispose(false);
+        }
+    }
+}
--- src/Extended.System/DelegateExtensions.cs
+++ src/Extended.System/DelegateExtensions.cs
@@ -19,5 +19,31 @@ namespace Extended.System
 
             return @delegate.Method.IsDefined(typeof(AsyncStateMachineAttribute), false);
         }
+
+        /// <summary>
+        /// Invokes the whith lock using the specified delegate.
+        /// </summary>
+        /// <param name="delegate">The delegate.</param>
+        /// <param name="lockObject">The lock object.</param>
+        /// <param name="args">The args.</param>
+        public static void InvokeWhithLock(this Delegate @delegate, object lockObject, params object[] args)
+        {
+            var hasLock = false;
+            try
+            {
+                Monitor.TryEnter(lockObject, ref hasLock);
+                if (!hasLock)
+                    return;
+
+                @delegate?.DynamicInvoke(args);
+            }
+            finally
+            {
+                if (hasLock)
+                {
+                    Monitor.Exit(lockObject);
+                }
+            }
+        }
     }
 }
