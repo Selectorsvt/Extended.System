c7b5c94e96b57fa5a0fdf7065e8109cbdc8630d4
Selectorsvt <selectorsvt@gmail.com>
2024-08-07 13:02:02 +0300

    Add ScheduledTimer

---
+++ src/Extended.System/ScheduledTimer.cs
@@ -0,0 +1,81 @@
+﻿using Timer = System.Timers.Timer;
+
+namespace Extended.System
+{
+    /// <summary>
+    /// The scheduled timer class.
+    /// </summary>
+    public sealed class ScheduledTimer : IDisposable
+    {
+        private Timer timer;
+
+        /// <summary>
+        /// The async delegate.
+        /// </summary>
+        public event Func<CancellationToken, Task>? AsyncDelegate;
+        private CancellationTokenSource? cancellationTokenSource;
+        private SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScheduledTimer"/> class.
+        /// </summary>
+        public ScheduledTimer()
+        {
+            timer = new Timer();
+            timer.Elapsed += SyncTimer_Elapsed;
+        }
+
+        /// <summary>
+        /// Syncs the timer elapsed using the specified sender.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The .</param>
+        private async void SyncTimer_Elapsed(object? sender, global::System.Timers.ElapsedEventArgs e)
+        {
+            var token = cancellationTokenSource!.Token;
+            timer.Stop();
+            await semaphoreSlim.WaitAsync(token).ConfigureAwait(false);
+            try
+            {
+                if (AsyncDelegate != null)
+                    await AsyncDelegate.Invoke(token).ConfigureAwait(false);
+                else
+                    throw new Exception($"{nameof(AsyncDelegate)} not initialized.");
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+                timer.Start();
+            }
+        }
+
+        /// <summary>
+        /// Starts the time span.
+        /// </summary>
+        /// <param name="timeSpan">The time span.</param>
+        public void Start(TimeSpan timeSpan)
+        {
+            timer.Interval = timeSpan.TotalMilliseconds;
+            cancellationTokenSource = new CancellationTokenSource();
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        public void Stop()
+        {
+            cancellationTokenSource?.Cancel();
+            timer.Stop();
+        }
+
+        /// <summary>
+        /// Disposes this instance.
+        /// </summary>
+        public void Dispose()
+        {
+            timer.Dispose();
+            cancellationTokenSource?.Dispose();
+        }
+    }
+}
