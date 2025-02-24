2b2936441a91836250d051c97bc71006c9618c95
Selectorsvt <selectorsvt@gmail.com>
2025-02-24 16:34:22 +0300

    Add linker time for assembly

--- src/Extended.System/AssemblyExtensions.cs
+++ src/Extended.System/AssemblyExtensions.cs
@@ -44,5 +44,36 @@ namespace Extended.System
 
             return assembly.Location;
         }
+
+        /// <summary>
+        /// Gets the linker time using the specified assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <param name="target">The target.</param>
+        /// <returns>The local time.</returns>
+        public static DateTime GetLinkerTime(this Assembly assembly, TimeZoneInfo? target = null)
+        {
+            assembly.CheckIsNull();
+
+            var filePath = assembly.Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+
+            var buffer = new byte[2048];
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                stream.Read(buffer, 0, 2048);
+
+            var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
+
+            var tz = target ?? TimeZoneInfo.Local;
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
+
+            return localTime;
+        }
     }
 }
